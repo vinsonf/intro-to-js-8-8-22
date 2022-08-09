6069f0f0c6dfcc7becf7edab0adec1b75cbfc7df
vinferno <vinsonfernandez@gmail.com>
2022-08-08 20:59:58 -0700

    day01

---
+++ day01/js/script.js
@@ -0,0 +1,38 @@
+no = false;
+no = true;
+yes = true; // boolean
+// boolean 
+
+one = 1;
+One = 1;
+// number
+
+abc = 'abc';
+a = 'a';
+// string
+
+// organization
+
+object = {
+    name: 'John',
+    age: 30,
+    isPresent: true
+}
+// object
+h1 = {
+    color: 'red',
+    fontSize: '20px'
+}
+
+array = [1, 2, 3, 4, 5];
+// array
+
+
+
+
+
+
+
+
+
+
