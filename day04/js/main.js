b61f05038c817817cc4b85a98decb5c02c258fd6
vinferno <vinsonfernandez@gmail.com>
2022-08-17 21:09:54 -0700

    day04

--- day04/js/main.js
+++ day04/js/main.js
@@ -48,12 +48,53 @@ else if (date.getHours() > 8) {
     h1.textContent = 'Too Early!';
 } 
 
-const age = prompt('What is your age?');
+// const age = prompt('What is your age?');
 
-if (age >= 18) {
-    alert('You are old enough to use this site!');
-} else if (age > 12 && confirm('Do you have your parents permission to use this site?')) {
-    alert('You are old enough to use this site!');
-} else {
-    alert('Sorry, you are too young to use this site!');
+// if (age >= 18) {
+//     alert('You are old enough to use this site!');
+// } else if (age > 12 && confirm('Do you have your parents permission to use this site?')) {
+//     alert('You are old enough to use this site!');
+// } else {
+//     alert('Sorry, you are too young to use this site!');
+// }
+
+
+const confirmBox = document.querySelector('.overlay');
+
+h1.addEventListener('click', () => {
+    // debugger;
+    confirmBox.classList.remove('hiddens');
+})
+
+const buttons = document.querySelectorAll('button');
+buttons[0].addEventListener('click', () => {
+    confirmBox.classList.add('hidden');
+    console.log('user clicked yes');
+})
+buttons[1].addEventListener('click', () => {
+    confirmBox.classList.add('hidden');
+    console.log('user clicked no');
+})
+buttons[2].addEventListener('click', () => {
+    confirmBox.classList.add('hidden');
+    console.log('user clicked maybe');
+})
+
+document.querySelector('.overlay').addEventListener('click', (e) => {
+    if (e.target.classList.contains('overlay')) {
+        confirmBox.classList.add('hidden');
+    }
+});
+
+
+
+function getValidEmail() {
+    const email = prompt('What is your email?');
+    if (email.length > 8 && email.includes('@') && email.includes('.')) {
+        alert('Thanks for sharing your email!');
+    } else {
+        alert('Please enter your email!');
+        getValidEmail();
+    }
 }
+getValidEmail();
