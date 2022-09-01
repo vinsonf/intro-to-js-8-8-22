cde8b5b433bc04c6e9cc06c7f843f04289421700
vinferno <vinsonfernandez@gmail.com>
2022-08-31 20:59:09 -0700

    day08

--- day08/js/main.js
+++ day08/js/main.js
@@ -3,8 +3,39 @@
 // import * as strings from './strings.js';
 // import * as math from './date-math-popups.js';
 // import * as controlFlow from './control-flow.js';
-import * as classes from './classes.js';
+// import * as classes from './classes.js';
+
+class Pokemon {
+  constructor(name) {
+    this.name = name;
+    this.createPokemon();
+  }
+  createPokemon() {
+    fetch("http://pokeapi.co/api/v2/pokemon/" + this.name)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        const img = document.createElement("img");
+        img.src = data.sprites.front_default;
 
 
+        document.body.appendChild(img);
+        this.height = data.height;
+
+
+        img.addEventListener('click', () => {
+            alert(this.height);
+        })
+      });
+  }
+}
+
+const pikachu = new Pokemon("pikachu");
+const charmander = new Pokemon("charmander");
+const squirtle = new Pokemon("squirtle");
+const bulbasaur = new Pokemon("bulbasaur");
+const pidgey = new Pokemon("pidgey");
 
 
